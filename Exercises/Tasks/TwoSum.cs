197c5a14ccd46c1afbc59ef24497fc0ba23c8fcd
p0vned <w.strzebonski@gmail.com>
2020-09-28 20:53:06 +0200

    Added TwoSum solution

---
+++ Exercises/Tasks/TwoSum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercises.Tasks
+{
+    public static class TwoSum
+    {
+        public static int[] Solution(int[] nums, int target)
+        {
+            var valueDictPosition = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var desiredValue = target - nums[i];
+
+                if (valueDictPosition.ContainsKey(desiredValue))
+                    return new int[] { i, valueDictPosition[desiredValue] };
+
+                valueDictPosition[nums[i]] = i;
+            }
+
+            throw new Exception("Not found");
+        }
+    }
+}
