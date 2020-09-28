c12112a2e263c5089ae5e6d69dcb94f525903f4f
p0vned <w.strzebonski@gmail.com>
2020-09-28 21:35:38 +0200

    Added the solution for Reverse problem

---
+++ Exercises/Tasks/Reverse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+
+namespace Exercises.Tasks
+{
+    public static class Reverse
+    {
+        //Given a 32-bit signed integer, reverse digits of an integer.
+        public static int Solution(int x)
+        {
+            var numberAsString = x.ToString();
+            string reversedString;
+
+            if (x >= 0)
+                reversedString = string.Join("", numberAsString.Reverse());
+            else
+            {
+                var reversedStringList = numberAsString.Remove(0, 1).Reverse().ToList();
+                reversedStringList.Insert(0, '-');
+
+                reversedString = string.Join("", reversedStringList);
+            }
+
+            int result;
+            bool isParseValid = int.TryParse(reversedString, out result);
+
+            //if (isParseValid)
+            return result;
+
+            //throw new Exception("Parsing failed");
+        }
+    }
+}
