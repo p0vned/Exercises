5039d3113f508f2a2f2628160e1b91c1b2217169
p0vned <strzebonsky@gmail.com>
2020-09-29 18:33:13 +0200

    Added PalindromeNumber solution

---
+++ Exercises/Tasks/PalindromeNumber.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace Exercises.Tasks
+{
+    public static class PalindromeNumber
+    {
+        public static bool Solution(int x)
+        {
+            if (x < 0)
+                return false;
+            
+            var numberAsString = x.ToString();
+            var numberReversed = string.Join(string.Empty, numberAsString.Reverse());
+
+            if (numberAsString.Equals(numberReversed))
+                return true;
+
+            return false;
+        }
+    }
+}
