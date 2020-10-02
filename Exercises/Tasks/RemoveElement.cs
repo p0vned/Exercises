488955707c27d713fcc0f84af30be5f3e4fdd73d
p0vned <strzebonsky@gmail.com>
2020-10-02 19:52:19 +0200

    Added RemoveElement solution

---
+++ Exercises/Tasks/RemoveElement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Text;
+
+namespace Exercises.Tasks
+{
+    public static class RemoveElement
+    {
+        /*
+        Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+        Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+        The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+         */
+
+        public static int Solution(int[] nums, int val)
+        {
+            if (nums.Length == 0) return 0;
+            
+            int l = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[l] = nums[i];
+                    l++;
+                }
+            }
+
+            return l;
+        }
+    }
+}
