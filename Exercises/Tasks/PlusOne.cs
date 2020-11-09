68a1b37a7acbe3b7031675f27e19c5dacf154814
p0vned <strzebonsky@gmail.com>
2020-11-09 20:09:40 +0100

    Added PlusOne solution

---
+++ Exercises/Tasks/PlusOne.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Exercises.Tasks
+{
+    public class PlusOne
+    {
+        public static int[] Solution(int[] digits)
+        {
+            int index = digits.Length - 1;
+
+            digits[index] += 1;
+
+            if (digits[index] != 10)
+                return digits;
+            
+            while (digits[index] == 10)
+            {
+                try
+                {
+                    digits[index] = 0;
+                    digits[index - 1] += 1;
+                    index--;
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    var newDigits = new int[digits.Length + 1];
+                    newDigits[0] = 1;
+
+                    for (int i = 1; i < newDigits.Length; i++)
+                    {
+                        newDigits[i] = 0;
+                    }
+
+                    return newDigits;
+                }
+            }
+
+            return digits;
+        }
+    }
+}
